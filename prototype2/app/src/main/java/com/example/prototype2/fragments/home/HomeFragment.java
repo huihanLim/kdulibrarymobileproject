e866119d470c062f6654f3440bb3c058f11015b9
huihanLim <72590509+huihanLim@users.noreply.github.com>
2020-10-13 09:00:37 +0800

    Add files via upload

---
+++ prototype2/app/src/main/java/com/example/prototype2/fragments/home/HomeFragment.java
@@ -0,0 +1,19 @@
+package com.example.prototype2.fragments.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.prototype2.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
